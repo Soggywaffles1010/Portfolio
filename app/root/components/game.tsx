32c24981e4b8169ec683217800d96cc4ddaf6a5d
Allen <arusman1987@gmail.com>
2024-12-05 22:11:46 +0800

    asdasd

--- app/root/components/game.tsx
+++ app/root/components/game.tsx
@@ -7,12 +7,12 @@ import Textbox from './main_components/textbox';
 
 const Game: React.FC = () => {
   return (
-    <div className="w-full h-full relative flex justify-center items-center rounded-md">
+    <div className="w-full h-1/4 relative flex justify-center items-center rounded-md">
       <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
         <Textbox />
       </div>
       <Spline
-        scene="https://prod.spline.design/FcXuavVowV4gyzkt/scene.splinecode" className='rounded-md'
+        scene="https://prod.spline.design/FcXuavVowV4gyzkt/scene.splinecode" className='rounded-md h-1/4'
       />
     </div>
   );
