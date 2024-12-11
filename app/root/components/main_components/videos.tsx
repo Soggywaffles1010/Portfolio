730b8b037ed91f02bb52937a2f84e7d316779f15
Allen <arusman1987@gmail.com>
2024-12-11 08:33:24 +0800

    fixing video

--- app/root/components/main_components/videos.tsx
+++ app/root/components/main_components/videos.tsx
@@ -1,11 +1,10 @@
 'use client';
+
 import React from 'react';
-import NextVideo from 'next-video';
 import Link from 'next/link';
-import { Asset } from 'next-video/dist/assets.js';
 
 interface VideosProps {
-  video: string | Asset; // Allow both string and Asset types
+  video: string; // Now, only a string path is expected
   title: string;
   description: string;
   link: string;
@@ -20,7 +19,8 @@ const Videos: React.FC<VideosProps> = ({ video, title, description, link, onMous
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
     >
-      <NextVideo src={video} className="w-full h-auto" controls  />
+      {/* Use regular <video> tag */}
+      <video src={video} className="" controls />
       <div className="mt-2">
         <h3 className="text-lg font-semibold">{title}</h3>
         <p className="text-sm text-gray-600">{description}</p>
